b6752a68b8fcbf70e6e492e5cc411dd2ebdac9eb
Kanae Kochigami <kochigami@jsk.imi.i.u-tokyo.ac.jp>
2018-05-31 11:25:06 +0900

    (wip) add comments & readme

---
+++ hw1/README.md
@@ -0,0 +1,41 @@
+# HW1
+
+## How to execute
+
+`python hw1.py`  
+
+Then, enter characters shown in apps
+Note: Please enter `q` when `Qu` appears.  
+
+ex. 
+```
+python hw1.py 
+
+Enter alphabets: moonstarer
+```
+Finally, you will get the results like this. 
+`I found: astronomer 11`
+
+Please enter the result (astronomer) in apps.  
+Enjoy! :)
+
+## My STEP
+
+### step 0
+1051 25m46s
+
+### step 1
+1020 4m8s
+1068 4m15s
+1264 3m48s
+1262 3m53s
+
+
+### step 2
+1438 3m20s
+
+### step 3
+1539 3m26s
+
+### step 4
+1728 3m26s
--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import itertools
 
 class DICTIONARY():
     def __init__(self):
         self.data = []
-        self.data_dict_sorted = {}
         self.data_dict = {}
         self.read_dictionary("dictionary.txt")
         self.create_dict()
-        self.sort_dict()
 
     def read_dictionary(self, dictionary_name):
         # 辞書の読み込み
@@ -19,7 +16,8 @@ class DICTIONARY():
         dict_data_tmp.close()
 
     def create_dict(self):
-        # separate by number of chars
+        # separate by number of chars of a word
+        # ex. "rabbit" enters data_dict[6]
         self.data_dict[2] = []
         self.data_dict[3] = []
         self.data_dict[4] = []
@@ -39,41 +37,21 @@ class DICTIONARY():
         self.data_dict[18] = []
 
         for word in self.data:
-            # 単語の分割．文字を一つずつ取り出す
+            # 単語の分割．文字を一つずつ取り出してリストに入れる
             chars = list(word.lower())
             # ex. chars: ['b', 'l', 'a', 'd', 'e', 's', '\n'] need to delete \n
             chars[len(chars)-1:]=[]
             self.data_dict[len(chars)].append(chars)
 
-    def sort_dict(self):
-        self.data_dict_sorted[2] = []
-        self.data_dict_sorted[3] = []
-        self.data_dict_sorted[4] = []
-        self.data_dict_sorted[5] = []
-        self.data_dict_sorted[6] = []
-        self.data_dict_sorted[7] = []
-        self.data_dict_sorted[8] = []
-        self.data_dict_sorted[9] = []
-        self.data_dict_sorted[10] = []
-        self.data_dict_sorted[11] = []
-        self.data_dict_sorted[12] = []
-        self.data_dict_sorted[13] = []
-        self.data_dict_sorted[14] = []
-        self.data_dict_sorted[15] = []
-        self.data_dict_sorted[16] = []
-        self.data_dict_sorted[17] = []
-        self.data_dict_sorted[18] = []
-        # ソートされた辞書の作成
-        for key in self.data_dict.keys():
-            for word in self.data_dict[key]:
-                word_sorted = sorted(word)
-                self.data_dict_sorted[len(word)].append(word_sorted)
-
+'''
+入力文字列 s_p に 辞書の単語 tag_p の各文字が含まれているかを調べる関数
+'''
 def find_common_char(tag_p, s_p):
     tag_p_cp = tag_p[:]
     s_p_cp = s_p[:]
     found_list = []
     count = 0
+    # len(tag_p_cp) =< len(s_cp_p) がこのプログラムでは常に成り立つので，tag_cp_pが先に空になる
     while tag_p_cp!=[]:
         count = 0
         for s in range(len(s_p_cp)):
@@ -88,26 +66,49 @@ def find_common_char(tag_p, s_p):
                 break
     return found_list
 
+'''
+s: 入力文字列　（ゲームで与えられる文字の列）
+count: sの長さ
+dictionary.data_dict: 辞書の単語をアルファベット順ではなく，文字の長さで分けたもの（辞書）
+dictionary.data_dict[i][word_count]: iは文字の長さ(2-18にした)，word_countはそのうち何番目か
+                                     ex. dictionary.data_dict[12][0] => ['a', 'f', 'r', 'o', 'c', 'e', 'n', 't', 'r', 'i', 's', 'm']
+intersection: s とdictionary.data_dict[i][word_count] で被っている文字のリスト
+              ex. s: ['m', 'o', 'o', 'n'] と dictionary.data_dict[i][word_count]: ['m', 'o', 'o', 'o', 'm'] なら，['m', 'o', 'o']
+candidate:     sを元に出来た単語の文字列リスト（最高点計算用）
+               ex. [['m', 'o', 'o', 'n'], [...], [...]]
+candidate_tag: sを元に出来た単語の文字列リスト（最終結果表示用）
+               ex. ['moon', '...', '...']
+point: candidateそれぞれの得点（正確な計算ではないが十分な計算）
+max_idx: 最高点を出すcandidateのインデックス
+'''
+
 if __name__ == '__main__':
     dictionary = DICTIONARY()
+    # 終了はCtrl+cで行う
     while True:
         # 文字の入力
         s = raw_input("Enter alphabets: ")
-        # sのソート
+        # sのアルファベットを全て小文字にして，リストに一つずつ入れる
         s = list(s.lower())
-        s.sort()
         count = len(s)
         candidate = []
         candidate_tag = []
         # 辞書の中の文字数が多いものから検索
         for i in range(count, 1, -1):
-            for word_count in range(len(dictionary.data_dict_sorted[i])):
-                intersection = find_common_char(dictionary.data_dict_sorted[i][word_count], s)
-                if len(intersection) > 0 and len(intersection) == len(dictionary.data_dict_sorted[i][word_count]):
+            for word_count in range(len(dictionary.data_dict[i])):
+                # 辞書の単語の文字が 入力文字列に含まれているか調べる
+                # 含まれている文字をリストに入れていく
+                intersection = find_common_char(dictionary.data_dict[i][word_count], s)
+                # もし上で出来たリストと辞書の単語のリストの長さが一緒なら，文字が全部含まれていることになる
+                if len(intersection) > 0 and len(intersection) == len(dictionary.data_dict[i][word_count]):
+                    # candidate: 最高点計算用の文字列リスト
+                    # ex. [['m', 'o', 'o', 'n'], [...], [...]]
                     candidate.append(intersection)
+                    # candidate_tag: 最終結果表示用の文字列リスト
+                    # ex. ['moon', '...', '...']
                     candidate_tag.append(''.join(dictionary.data_dict[i][word_count]))
 
-        # 点数の高いものを選ぶ
+        # candidateから点数の高いものを選ぶ
         point = []
         for word in candidate:
             tmp = 0
@@ -120,5 +121,8 @@ if __name__ == '__main__':
                     tmp+=1
             point.append(tmp)
 
-        max_idx = point.index(max(point))
-        print ("I found: {} {}").format(candidate_tag[max_idx], max(point))
+        if len(point) > 0:
+            max_idx = point.index(max(point))
+            print ("I found: {} {}").format(candidate_tag[max_idx], max(point))
+        else:
+            print "PASS"
